461790bf7ee99bae9315717ee9f6991c07137a69
Kay Strobach <github@kay-strobach.de>
2013-03-13 21:15:03 +0100

    refactoring

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,5 @@
 <?php
 
-$scssParserConfig = unserialize($TYPO3_CONF_VARS['EXT']['extConf'][$_EXTKEY]);
-
-
 tx_DynCss_Configuration_BeRegistry::get()->registerFileHandler('sass', 'tx_DyncssPhpsass_ParserSass');
 tx_DynCss_Configuration_BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssPhpsass_ParserScss');
 
-
-unset($scssParserConfig);
