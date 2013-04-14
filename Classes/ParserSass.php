7b5e5e9c2b0ba4469f0475e8c369ec0fd2b61990
Kay Strobach <github@kay-strobach.de>
2013-04-14 19:00:26 +0200

    [BUGFIX] Fixes Problem including external SASS file via import

--- Classes/ParserSass.php
+++ Classes/ParserSass.php
@@ -70,8 +70,8 @@ class tx_DyncssPhpsass_ParserSass extends tx_Dyncss_Parser_AbstractParser{
 	 */
 	protected function _compileFile($inputFilename, $preparedFilename, $outputFilename, $cacheFilename) {
 		try {
-			$this->parser->load_paths[] = dirname($inputFilename) . '/';
-			$this->parser->load_paths[] = PATH_site;
+			$this->parser->load_paths[] = dirname($inputFilename);
+
 			return $this->parser->toCss($preparedFilename);
 		} catch(Exception $e) {
 			return $e;
