0775d2c97987beb60e2ec8b2bb990bdfbec62a59
smadsen <soren@picment.com>
2015-03-23 13:26:40 +0100

    Update ext_localconf.php
    [BUG] Fix small typo

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,4 @@
 <?php
 
-\KayStrobach\DynCss\Configuration\BeRegistry::get()->registerFileHandler('sass', 'tx_DyncssPhpsass_ParserSass');
-\KayStrobach\DynCss\Configuration\BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssPhpsass_ParserScss');
-
+\KayStrobach\Dyncss\Configuration\BeRegistry::get()->registerFileHandler('sass', 'tx_DyncssPhpsass_ParserSass');
+\KayStrobach\Dyncss\Configuration\BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssPhpsass_ParserScss');
